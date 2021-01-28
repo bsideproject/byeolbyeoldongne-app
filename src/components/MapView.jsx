57a295789d79590529697bbc058b171a95fbd87c
Park hanjun <park.hanjun00@gmail.com>
2021-01-28 19:33:05 +0900

    :abc: ADD eslint & prettier 기본 세팅 완료

--- src/components/MapView.jsx
+++ src/components/MapView.jsx
@@ -1,20 +1,25 @@
-import React, {useEffect} from 'react';
-import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
+import React, { useEffect } from 'react';
+import { StyleSheet } from 'react-native';
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
 import Geolocation from '@react-native-community/geolocation';
 
 const GoogleMap = (props) => {
-  useEffect(() => {
-    Geolocation.getCurrentPosition((position) => console.log(position));
-  }, []);
+    useEffect(() => {
+        Geolocation.getCurrentPosition((position) => console.log(position));
+    }, []);
 
-  return (
-    <MapView
-      {...props}
-      style={{width: '100%', height: '100%'}}
-      mapType="standard"
-      provider={PROVIDER_GOOGLE}
-    />
-  );
+    return (
+        <MapView
+            {...props}
+            style={styles.mapView}
+            mapType="standard"
+            provider={PROVIDER_GOOGLE}
+        />
+    );
 };
+
+const styles = StyleSheet.create({
+    mapView: { width: '100%', height: '100%' },
+});
 
 export default GoogleMap;
