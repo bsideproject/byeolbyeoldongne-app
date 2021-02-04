30335aa9157148555f42cabbc5fb23270116ea63
정찬호 <lovgone@naver.com>
2021-02-04 18:36:27 +0900

    Login 스크린추가

--- src/navigation/Route.jsx
+++ src/navigation/Route.jsx
@@ -4,6 +4,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 import MainScreen from "../screens/Main";
 import SettingScreen  from "../screens/Settings";
 import HomeScreen from '../screens/Home';
+import LoginScreen from '../screens/Login';
 
 
 const Stack = createStackNavigator();
@@ -11,7 +12,8 @@ const Stack = createStackNavigator();
 const Route = () =>{
   return (
     <NavigationContainer>
-        <Stack.Navigator initialRouteName="Home" headerMode="none">
+        <Stack.Navigator initialRouteName="Login" headerMode="none">
+            <Stack.Screen name="Login" component={LoginScreen}/>
             <Stack.Screen name="Home" component={HomeScreen}/>
             <Stack.Screen name="Main" component={MainScreen}/>
             <Stack.Screen name="Settings" component={SettingScreen}/>
